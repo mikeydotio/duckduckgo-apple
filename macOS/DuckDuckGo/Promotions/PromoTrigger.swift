a6e3d8006418f6c23ddae698b8bbe732bfcee64f
Rachel McRoberts <rmcroberts@duckduckgo.com>
2026-03-03 18:00:23 +0000

    Add new Promo types

---
+++ macOS/DuckDuckGo/Promotions/PromoTrigger.swift
@@ -0,0 +1,27 @@
+//
+//  PromoTrigger.swift
+//
+//  Copyright © 2026 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+/// Events that can trigger a promo.
+///
+/// Triggers should map to e.g. an `NSNotification` or `@Published` property
+/// that can be subscribed to by the PromoService.
+enum PromoTrigger {
+    case appLaunched
+    case windowBecameKey
+    case newTabPageAppeared
+}
