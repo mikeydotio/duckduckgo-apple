33416579ffd4cb3b1af49186307c7bd49c4baee6
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2026-03-03 14:52:25 -0300

    Do not let suspend tabs with active forms

--- macOS/DuckDuckGo/Tab/Model/Tab.swift
+++ macOS/DuckDuckGo/Tab/Model/Tab.swift
@@ -566,6 +566,7 @@ protocol TabDelegate: ContentOverlayUserScriptDelegate {
     // MARK: - Tab Suspension
 
     @Published private(set) var isSuspended: Bool = false
+    @Published private(set) var hasActiveFormInput: Bool = false
 
     var audioStatePublisher: AnyPublisher<WebView.AudioState, Never> {
         webView.audioStatePublisher
@@ -1277,6 +1278,7 @@ extension Tab: UserContentControllerDelegate {
 
         userScripts.debugScript.instrumentation = instrumentation
         userScripts.pageObserverScript.delegate = self
+        userScripts.formFocusUserScript.delegate = self
         userScripts.serpSettingsUserScript?.delegate = self
         userScripts.serpSettingsUserScript?.webView = self.webView
         specialPagesUserScript = nil
@@ -1290,6 +1292,15 @@ extension Tab: PageObserverUserScriptDelegate {
     func pageDOMLoaded() {
         loadedPageDOMPublisher.send()
         delegate?.tabPageDOMLoaded(self)
+    }
+
+}
+
+extension Tab: FormFocusUserScriptDelegate {
+
+    @MainActor
+    func formFocusUserScript(_ script: FormFocusUserScript, didChangeFocus focused: Bool) {
+        hasActiveFormInput = focused
     }
 
 }
@@ -1363,6 +1374,7 @@ extension Tab/*: NavigationResponder*/ { // to be moved to Tab+Navigation.swift
         }
 
         hasCommittedContent = true
+        hasActiveFormInput = false
     }
 
     @MainActor
