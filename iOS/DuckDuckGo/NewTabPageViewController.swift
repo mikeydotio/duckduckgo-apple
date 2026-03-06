cd048388cea30f7036be1cfac81930223f37a023
József Vesza <jvesza@duckduckgo.com>
2026-03-06 21:36:27 +0100

    Wait for avaialble products to load

--- iOS/DuckDuckGo/NewTabPageViewController.swift
+++ iOS/DuckDuckGo/NewTabPageViewController.swift
@@ -21,6 +21,7 @@ import SwiftUI
 import DDGSync
 import Bookmarks
 import BrowserServicesKit
+import Combine
 import Core
 import RemoteMessaging
 
@@ -51,6 +52,7 @@ final class NewTabPageViewController: UIHostingController<NewTabPageView>, NewTa
     private let appWidthObserver: AppWidthObserver
 
     private let internalUserCommands: URLBasedDebugCommands
+    private var cancellables = Set<AnyCancellable>()
 
     var onViewDidAppear: (() -> Void)?
 
@@ -116,6 +118,7 @@ final class NewTabPageViewController: UIHostingController<NewTabPageView>, NewTa
         super.viewDidLoad()
 
         registerForNotifications()
+        subscribeToProductAvailability()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -250,6 +253,20 @@ final class NewTabPageViewController: UIHostingController<NewTabPageView>, NewTa
 
     private func presentNextDaxDialog() {
         showNextDaxDialogNew(dialogProvider: daxDialogsManager, factory: newTabDialogFactory)
+    }
+
+    private func subscribeToProductAvailability() {
+        AppDependencyProvider.shared.subscriptionManager.hasAppStoreProductsAvailablePublisher
+            .removeDuplicates()
+            .filter { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                guard let self,
+                      self.view.window != nil,
+                      self.hostingController == nil else { return }
+                self.presentNextDaxDialog()
+            }
+            .store(in: &cancellables)
     }
 
     // MARK: -
