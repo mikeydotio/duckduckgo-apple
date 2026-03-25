4a8dea45e954cf1117471aba770b82a8e9732bad
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-03-25 11:40:10 -0300

    MouseOverButton: Animating mouseOver Tinting

--- macOS/DuckDuckGo/Common/View/AppKit/MouseOverButton.swift
+++ macOS/DuckDuckGo/Common/View/AppKit/MouseOverButton.swift
@@ -42,6 +42,10 @@ internal class MouseOverButton: NSButton, Hoverable {
     @IBInspectable dynamic var backgroundInset: NSPoint = .zero
     @IBInspectable dynamic var mustAnimateOnMouseOver: Bool = false
 
+    private enum Animations {
+        static let duration: TimeInterval = 0.15
+    }
+
     @IBInspectable var mouseOverTintColor: NSColor? {
         didSet {
             updateTintColor()
@@ -179,15 +183,29 @@ internal class MouseOverButton: NSButton, Hoverable {
     }
 
     func updateTintColor() {
-        NSAppearance.withAppAppearance {
-            if isMouseDown {
-                self.contentTintColor = self.mouseDownTintColor ?? self.normalTintColor
-            } else if isMouseOver {
-                self.contentTintColor = self.mouseOverTintColor ?? self.normalTintColor
-            } else {
-                self.contentTintColor = self.normalTintColor
+        guard mustAnimateOnMouseOver && !isMouseDown else {
+            NSAppearance.withAppAppearance {
+                self.contentTintColor = currentTintColor()
             }
+            return
         }
+
+        NSAnimationContext.runAnimationGroup { context in
+            context.duration = Animations.duration
+            self.animator().contentTintColor = currentTintColor()
+        }
+    }
+
+    private func currentTintColor() -> NSColor? {
+        if isMouseDown {
+            return mouseDownTintColor ?? normalTintColor
+        }
+
+        if isMouseOver {
+            return mouseOverTintColor ?? normalTintColor
+        }
+
+        return normalTintColor
     }
 
     private var hoverTrackingArea: HoverTrackingArea? {
