61b57d25b69af0c62cfc3f95dfee0fb8cdf0c899
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-03-26 23:21:33 +0100

    Update SuggestionProcessorRust to 0.1.0

--- SharedPackages/SuggestionProcessing/Package.swift
+++ SharedPackages/SuggestionProcessing/Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SuggestionsProcessorRust",
-            path: "artifacts/SuggestionsProcessorRust.xcframework"
+            url: "https://github.com/ayoy/suggestions_processor/releases/download/0.1.0/SuggestionsProcessorRust.xcframework.zip",
+            checksum: "11d7dae585296103f7962553dc99a1d2fc240e3c68c97e9ed2f9614ce3a6a56a"
         ),
         .testTarget(
             name: "SuggestionProcessingTests",
