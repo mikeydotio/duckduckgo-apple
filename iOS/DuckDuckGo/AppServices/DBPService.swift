1a3efc3cb8795695ec90512e3d638e4ab2974cfd
Anh Do <ado@duckduckgo.com>
2026-07-18 17:26:42 -0400

    Add feature flag

--- iOS/DuckDuckGo/AppServices/DBPService.swift
+++ iOS/DuckDuckGo/AppServices/DBPService.swift
@@ -29,7 +29,6 @@ import Subscription
 import os.log
 
 final class DBPService: NSObject {
-    private let shouldDeferSecureVaultInitialization = true
     private let dbpIOSManager: DataBrokerProtectionIOSManager?
     public let freemiumDBPUserStateManager: FreemiumDBPUserStateManaging
     public let profileStateManager: DBPProfileStateManaging
@@ -127,7 +126,7 @@ final class DBPService: NSObject {
                 isWebViewInspectable: isWebViewInspectable,
                 freeTrialConversionService: appDependencies.freeTrialConversionService,
                 contentBlocking: dbpContentBlocking,
-                shouldDeferSecureVaultInitialization: shouldDeferSecureVaultInitialization)
+                shouldDeferSecureVaultInitialization: appDependencies.featureFlagger.isFeatureOn(.dbpDeferredSecureVaultInit))
         } else {
             assertionFailure("PixelKit not set up")
             self.dbpIOSManager = nil
